b2cec2489cc8f07822f014f8b639e491d8401bb7
Douglas Berdeaux <weaknetlabs@gmail.com>
2022-02-04 13:52:56 -0500

    Added ok() and fail() constructs

--- classes/Color.py
+++ classes/Color.py
@@ -1,10 +1,19 @@
 from sty import fg
 class Color:
     def __init__(self):
-        self.RED=fg(197)
-        self.GRN=f"\033[3m{fg(201)} ✔ "
-        self.YLL=fg(226)
-        self.RST='\033[0m'
-        self.LMGE='\033[95m'
-        self.CMNT='\033[37m\033[3m'
-        self.OKGRN=f"\033[3m{fg(46)} ✔ "
+        self.RED=fg(197) # pretty red
+        self.YLL=fg(226) # pretty yellow
+        self.RST='\033[0m' # reset the color to terminal default
+        self.LMGE='\033[95m' # light magenta
+        self.CMNT='\033[37m\033[3m' # comment-like text
+
+    def arrow(self):
+        return f"{self.CMNT}{self.RED}-->{self.CMNT}"
+    def ques(self):
+        return f"{self.LMGE}[{self.RED}?{self.LMGE}]{self.RST}"
+    def info(self):
+        return f"{self.LMGE}[{self.RED}i{self.LMGE}]{self.RST}"
+    def ok(self,msg):
+        print(f"\033[3m{fg(201)} ✔ {msg}{self.RST}")
+    def fail(self,msg):
+        print(f"\033[3m{fg(197)} ✖ {msg}{self.RST}")
