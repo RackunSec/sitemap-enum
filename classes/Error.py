cc06b798865c7fc32fce44f642271cf3cca29f98
Douglas Berdeaux <weaknetlabs@gmail.com>
2022-02-04 13:52:28 -0500

    Destroyed Error class and moved into Color

--- classes/Error.py
+++
@@ -1,7 +0,0 @@
-from classes.Color import Color
-# Display errors:
-class Error():
-    def __init__(self):
-        self.color = Color() # store the object
-    def say(self,msg):
-        print(f"{self.color.RED} ERROR: {msg}{self.color.RST}")
