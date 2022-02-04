22348b462aea4bac3996b2bb4392f6586a52cd9c
Douglas Berdeaux <weaknetlabs@gmail.com>
2022-02-04 13:58:35 -0500

    Added app banner for reuse in apps

--- classes/Color.py
+++ classes/Color.py
@@ -7,13 +7,25 @@ class Color:
         self.LMGE='\033[95m' # light magenta
         self.CMNT='\033[37m\033[3m' # comment-like text
 
-    def arrow(self):
+    def arrow(self): # This is just " --> " but fancyier.
         return f"{self.CMNT}{self.RED}-->{self.CMNT}"
-    def ques(self):
+    def ques(self): # This just asks "[?]" much fancier:
         return f"{self.LMGE}[{self.RED}?{self.LMGE}]{self.RST}"
-    def info(self):
+    def info(self): # This just says "[i]" much fancier:
         return f"{self.LMGE}[{self.RED}i{self.LMGE}]{self.RST}"
-    def ok(self,msg):
+    def ok(self,msg): # This just says "[ok]" much fancier:
         print(f"\033[3m{fg(201)} ✔ {msg}{self.RST}")
-    def fail(self,msg):
+    def fail(self,msg): # THis handles all failures during runtime
         print(f"\033[3m{fg(197)} ✖ {msg}{self.RST}")
+    def banner(self): # Place ASCII art and app title:
+        print(f"""
+    {self.RED}
+
+     oooooooo8 oooo     oooo ooooooooooo
+    888         8888o   888   888    88
+     888oooooo  88 888o8 88   888ooo8
+            888 88  888  88   888    oo
+    o88oooo888 o88o  8  o88o o888ooo8888
+    {self.YLL}
+    Sitemap Enumerator{self.RST}
+        """)
