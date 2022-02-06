957127c2079167d8700111fa7b24fbf7d78f8935
Douglas Berdeaux <weaknetlabs@gmail.com>
2022-02-05 21:17:28 -0500

    Pythonic change using Tuple

--- sitemap-enum.py
+++ sitemap-enum.py
@@ -4,12 +4,13 @@
 # 2022 Douglas Berdeaux
 #
 
-import sys # get args from user
-import re # matching
-import os # for file creation
-from bs4 import BeautifulSoup # for XML parsing
+import sys  # get args from user
+import re  # matching
+import os  # for file creation
+from bs4 import BeautifulSoup  # for XML parsing
 from classes.Style import Style
 from classes.Http import Http
+
 
 def main(args):
     style = Style()
@@ -20,30 +21,36 @@ def main(args):
         # set up variables:
         http = Http()
         # grab the URL:
-        if len(args)==2:
+        if len(args) == 2:
             url = args[1]
-        else: # determine the URL
+        else:  # determine the URL
             for arg in args:
-                if(re.match("^http.*\.xml",arg)):
+                if re.match("^http.*\.xml", arg):
                     url = arg
         # Check URL for correctedness:
-        if re.match("^http.*\.xml",url):
-            try: # scrape the sitemap first:
-                sites = http.getxml(url) # make HTTP request for sitemap
-                if len(args)==3: # scrape each individual link from the sitemap:
-                    if args[1]=="--scrape" or args[2]=="--scrape":
+        if re.match("^http.*\.xml", url):
+            try:  # scrape the sitemap first:
+                sites = http.getxml(url)  # make HTTP request for sitemap
+                if len(args) == 3:  # scrape each individual link from the sitemap:
+                    if "--scrape" in (args[1],args[2]): # "Pythonic"
                         i = 0
-                        for url in sites: # loop over all URLs pulled from XML file
-                            i=i+1 # token for math below:
-                            percentdone = round((i/len(sites))*100,2)
-                            print(f"{style.CMNT}Scraping sites:{style.RED} {percentdone}% {style.LMGE}{i}{style.CMNT}/{style.LMGE}{len(sites)}{style.RST} ...                        \r",end="")
+                        for url in sites:  # loop over all URLs pulled from XML file
+                            i = i + 1  # token for math below:
+                            percentdone = round((i / len(sites)) * 100, 2)
+                            print(
+                                f"{style.CMNT}Scraping sites:{style.RED} {percentdone}% {style.LMGE}{i}{style.CMNT}/{style.LMGE}{len(sites)}{style.RST} ...                        \r",
+                                end="",
+                            )
                             http.getCode(url)
-                        style.ok(f"Scraping completed.                             {style.RST}")
-            except Exception as e: # let the user know what happened:
+                        style.ok(
+                            f"Scraping completed.                             {style.RST}"
+                        )
+            except Exception as e:  # let the user know what happened:
                 exc_type, exc_obj, exc_tb = sys.exc_info()
                 style.fail(f"Something went wrong: {e} {exc_tb.tb_lineno}")
-        else: # Not a .xml file and URL:
+        else:  # Not a .xml file and URL:
             style.fail(f"Malformed XML URL: {args[1]}")
 
-if __name__ == "__main__": # Our main function.
+
+if __name__ == "__main__":  # Our main function.
     main(sys.argv)
